bdc94135e7ff6e292e60364132a199ed126d0846
Riley Allred <riley_allred@hotmail.com>
2023-05-17 21:17:50 -0600

    added base level routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,57 @@ import Header from './components/Header';
 import Footer from "./components/Footer"
 import Scan from './components/Scan';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import './output.css';
 
+const httpLink = createHttpLink({
+  uri: '/graphql'
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token')
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <main>
-        <Routes>
-          <Route
-            path= '/'
-            element= {<Scan/>}/>
-        </Routes>
-      </main>
-
-      <Footer />
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route
+              path='/'
+              element={<Scan />} />
+            <Route
+              path='/login'
+              element={"TODO: ADD LOGIN"} />
+            <Route
+              path='/signup'
+              element={"TODO: ADD SIGNUP"} />
+            <Route
+              path='/profile'
+              element={"TODO: ADD PROFILE"} />
+            <Route
+              path='/idle-events/:idleEventId'
+              element={'TODO: ADD IDLE EVENT PAGE'} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </ApolloProvider>
   );
 };
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const User = require('./schemas/userSchema');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
