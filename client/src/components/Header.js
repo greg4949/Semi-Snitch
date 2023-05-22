e82cece8495c2de2d7f747069af0c8d723500eff
Kole Dallam <kolejasonD@gmail.com>
2023-05-21 22:40:48 -0600

    Mobile functionality and hamburger menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,26 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Header = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const menuItems = [
+    { label: 'Log In', to: '/login' },
+    { label: 'Sign Out', to: '#!' },
+    { label: 'Upload Data', to: '/services' },
+  ];
+
+  const sortedMenuItems = [...menuItems].sort((a, b) => a.label.length - b.label.length);
+
   return (
     <header className="bg-gray-800">
       <nav className="container mx-auto px-4 py-3 flex items-center justify-between">
         <Link to="/">
-          <img src={logo} alt="Logo" style={{ height: '72px', width: 'auto', marginRight: '2in' }}/>
-          <span className="sr-only">Semi Snitch</span> 
+          <img src={logo} alt="Logo" style={{ height: '72px', width: 'auto' }} />
         </Link>
-        <ul className="flex space-x-4">
-          <li><Link to="/login"  className="text-gray-300 hover:text-white">Log In</Link></li>
-          {/* <li><Link to="/s" className="text-gray-300 hover:text-white">Sign Up</Link></li> */}
-
-          <li><Link to="#!" className="text-gray-300 hover:text-white">Sign Out</Link></li>
-          <li><Link to="/services" className="text-gray-300 hover:text-white">Upload Data</Link></li>
+        <div className="md:hidden">
+          <button
+            className="text-white focus:outline-none focus:text-white"
+            onClick={toggleMobileMenu}
+            aria-label="Toggle mobile menu"
+          >
+            <svg
+              className={`w-6 h-6 ${isMobileMenuOpen ? 'hidden' : 'block'}`}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+            <svg
+              className={`w-6 h-6 ${isMobileMenuOpen ? 'block' : 'hidden'}`}
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <ul
+          className={`md:flex space-x-4 ${
+            isMobileMenuOpen ? 'block text-center' : 'hidden'
+          }`}
+        >
+          {sortedMenuItems.map((menuItem) => (
+            <li key={menuItem.label}>
+              <Link
+                to={menuItem.to}
+                className="text-gray-300 hover:text-white"
+                onClick={toggleMobileMenu}
+              >
+                {menuItem.label}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
   );
-}
+};
 
 export default Header;
-
